b4578f2b313524f0fd807f0e5c29477521a994ab
tracer-ics2015 <tracer@njuics.org>
2016-07-25 09:51:07 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  09:51:07 up  9:16,  3 users,  load average: 0.00, 0.01, 0.05 3996b62c42933e65d1ff6e4052f03177d3aa97de

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -60,9 +60,8 @@ static int cmd_x(char *args) {
 	char *arg2 = strtok(NULL, " ");
 	int length = atoi(arg1);
 	int addr;
-	sscanf(arg2, "%x", &addr); 
-	assert(addr == strtol(arg2,NULL,16));
-	assert(addr == 1);
+	//sscanf(arg2, "%x", &addr); 
+	addr = strtol(arg2,NULL,16);
 	int i = 0;
 	for(i = 0; i < length; i++){
 		printf("0x%x:\t%08x\n", addr + 4 * i, swaddr_read(addr + 4 * i, 4 ));
