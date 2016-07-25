50730339a45322c0031309af725c065e4585de7c
tracer-ics2015 <tracer@njuics.org>
2016-07-26 02:17:15 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  02:17:15 up 1 day,  1:42,  3 users,  load average: 0.00, 0.01, 0.05 518381654ef6ae9b8e2a265644d8b2e2a59c42ae

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -81,7 +81,7 @@ static int cmd_q(char *args) {
 
 static int cmd_w(char *args) {
 	WP * newwp = new_wp();
-	strcpy(newwp->str, args);
+	//strcpy(newwp->str, args);
 	// Log("new wp's str is %s", newwp->str);
 	bool succ = true;
 	newwp->value = expr(newwp->str, &succ); 
