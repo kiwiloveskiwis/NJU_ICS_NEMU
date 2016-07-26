e284d6302b8686ae68fad7190ea2fbe8f5073827
tracer-ics2015 <tracer@njuics.org>
2016-07-26 14:27:29 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  14:27:29 up 1 day, 13:52,  3 users,  load average: 0.00, 0.01, 0.05 da732f561d5f6c1bbbdbd7b3f2aa521cc222507

---
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -0,0 +1,21 @@
+#include "cpu/exec/template-start.h"
+
+#define instr sub
+
+static void do_execute() {
+	DATA_TYPE result = op_dest->val - op_src->val;
+	OPERAND_W(op_dest, result);
+
+	// update EFLAGS
+	print_asm_template2();
+}
+
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
+
+#include "cpu/exec/template-end.h"
