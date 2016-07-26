4111cd7e14e2a262039254a8924803a1a9b3c9c6
tracer-ics2015 <tracer@njuics.org>
2016-07-26 22:40:35 +0800

    >  run 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  22:40:35 up 1 day, 22:05,  3 users,  load average: 0.00, 0.01, 0.05 31251d8a1e5d35deaed565e9d9c521d39f0a45c7

--- testcase/src/mytest.c
+++ testcase/src/mytest.c
@@ -4,16 +4,16 @@ int A[10];
 int b;
 
 int main() {
-	A[0] = 0;
+	A[0] = -2;
 	A[1] = -1;
 	A[2] = 2;
 	A[3] = 3;
 
 	b = A[3];
-	A[4] = b - A[1] + A[3];
+	A[4] = b - A[1] + A[3] + A[0] + b;
 
 	nemu_assert(A[3] == 3);
-	nemu_assert(A[4] == 7);
+	nemu_assert(A[4] == 8);
 	nemu_assert(b == 3);
 
 	HIT_GOOD_TRAP;
