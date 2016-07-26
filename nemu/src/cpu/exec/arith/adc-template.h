6ebcfb2b67756c9220c114e78e4a23c3fa6070a3
tracer-ics2015 <tracer@njuics.org>
2016-07-26 22:44:17 +0800

    >  run 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  22:44:17 up 1 day, 22:09,  3 users,  load average: 0.00, 0.01, 0.05 78314e94708e6426c848786a53a9846c51243533

---
+++ nemu/src/cpu/exec/arith/adc-template.h
@@ -0,0 +1,24 @@
+#include "cpu/exec/template-start.h"
+
+#define instr adc
+
+static void do_execute() {
+	DATA_TYPE result = op_dest->val + op_src->val + cpu.CF;
+	OPERAND_W(op_dest, result);
+	cpu.CF = (((unsigned long long)op_dest->val + (unsigned long long)op_src->val ) >> 32) & 1;
+	cpu.OF = (MSB(op_dest->val) ==  MSB(op_src->val)) && (MSB(op_src->val) != MSB(result));
+	update_PZS(result)
+
+	print_asm_template2();
+}
+
+	make_instr_helper(i2a)
+	make_instr_helper(i2rm)
+	make_instr_helper(r2rm)
+	make_instr_helper(rm2r)
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+	make_instr_helper(si2rm)
+#endif
+
+#include "cpu/exec/template-end.h"
+
