c7cb4a35e203626bcebf3592ec2ff3960b60b460
tracer-ics2015 <tracer@njuics.org>
2016-07-26 10:22:07 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  10:22:07 up 1 day,  9:47,  3 users,  load average: 0.08, 0.03, 0.05 6ea1322da74f0319f94d4234b655291eb1726cd0

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -14,19 +14,46 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  * For more details about the register encoding scheme, see i386 manual.
  */
 
-typedef union {
-	union {
-		uint32_t _32;
-		uint16_t _16;
-		uint8_t _8[2];
-	} gpr[8];
-
-	/* Do NOT change the order of the GPRs' definitions. */
-
-	struct {
-		uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-		swaddr_t eip;
+typedef struct {
+	union{
+		union {
+			uint32_t _32;
+			uint16_t _16;
+			uint8_t _8[2];
+		} gpr[8];
+		/* Do NOT change the order of the GPRs' definitions. */
+		struct {
+			uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+		};
 	};
+	swaddr_t eip;
+
+	union {
+		uint32_t EFLAGS;
+		struct {
+			uint32_t CF		:1;
+			uint32_t		:1;
+			uint32_t PF		:1;
+			uint32_t		:1;
+			uint32_t AF		:1;
+			uint32_t 		:1;
+			uint32_t ZF		:1;
+			uint32_t SF		:1;
+			uint32_t TF		:1;
+			uint32_t IF		:1;
+			uint32_t DF		:1;
+			uint32_t OF		:1;
+			uint32_t IOPL	:2;
+			uint32_t NT		:1;
+			uint32_t		:1;
+			uint32_t RF		:1;
+			uint32_t VM		:1; // 18 bits taken
+		};
+	};
+
+
+
+
 
 } CPU_state;
 
