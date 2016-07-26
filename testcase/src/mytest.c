f818d6ad96e9e1a8c117208debbe0c392322bbb5
tracer-ics2015 <tracer@njuics.org>
2016-07-26 21:33:33 +0800

    >  run 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  21:33:33 up 1 day, 20:58,  3 users,  load average: 0.02, 0.02, 0.05 c3f4c8ae0d245c2c0e43b22177ebdcc9d940729

--- testcase/src/mytest.c
+++ testcase/src/mytest.c
@@ -12,9 +12,6 @@ int main() {
 	b = A[3];
 	A[4] = b - A[1];
 
-	nemu_assert(A[0] == 0);
-	nemu_assert(A[1] == 1);
-	nemu_assert(A[2] == 2);
 	nemu_assert(A[3] == 3);
 	nemu_assert(A[4] == 4);
 	nemu_assert(b == 3);
