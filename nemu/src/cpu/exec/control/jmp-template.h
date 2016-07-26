36262a78d485d40b1ef57491c325447470b572b1
tracer-ics2015 <tracer@njuics.org>
2016-07-27 02:40:43 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  02:40:43 up 2 days,  2:05,  3 users,  load average: 0.00, 0.01, 0.05 20b9c77f59ad022b114f8c88a1c8b7b4cdd77a3

--- nemu/src/cpu/exec/control/jmp-template.h
+++ nemu/src/cpu/exec/control/jmp-template.h
@@ -6,12 +6,12 @@ static void do_execute(){
 		DATA_TYPE inc_adr = op_src->val ;
 		cpu.eip += inc_adr;
 		if(DATA_BYTE == 2) cpu.eip &= 0x0000ffff;
+		print_asm("jmp $0x%x", cpu.eip + DATA_BYTE + 1);
 	} else {
 		// jmp_rm_v, mod==3
 		if(DATA_BYTE == 2) cpu.eip = (op_src->val & 0x0000ffff) - DATA_BYTE;
 		else cpu.eip = op_src->val - DATA_BYTE;
 	}
-	print_asm_template1();
 }
 
 make_instr_helper(i)
