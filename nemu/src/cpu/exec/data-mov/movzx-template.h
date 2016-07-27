055a867735f7d57dafdcca7532f4fc0a5162379a
tracer-ics2015 <tracer@njuics.org>
2016-07-28 00:13:39 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  00:13:39 up 2 days, 23:38,  3 users,  load average: 0.00, 0.01, 0.05 36d8cc1ef68ae8e68e89bd068e4e517dbf0d4817

--- nemu/src/cpu/exec/data-mov/movzx-template.h
+++ nemu/src/cpu/exec/data-mov/movzx-template.h
@@ -5,7 +5,6 @@
 //   1001c9:	0f b6 45 0c				movzbl 0xc(%ebp),%eax	string.c
 //   8048841:	0f b6 c0				movzbl %al,%eax
 static void do_execute(){
-	op_src->val = (DATA_TYPE) op_src->val;
 
 	OPERAND_W(op_dest, op_src->val);
 	print_asm_template2();
