8864695df5c0fdda990881ebaddcb36d691ad495
tracer-ics2015 <tracer@njuics.org>
2016-07-28 06:13:05 +0800

    >  run 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  06:13:05 up 3 days,  5:38,  3 users,  load average: 0.00, 0.01, 0.05 d102743137f5d9f18f99b49357b31553287cee3

--- testcase/src/mytest.c
+++ testcase/src/mytest.c
@@ -12,12 +12,20 @@ struct dummy fun(struct dummy a) {
 }
 
 int main() {
-	d.pad1[0] = 128;
-	d.pad2[0] = 0;
-	struct dummy t = fun(d);// got wrong
+	int i;
+	for(i = 0; i < N; i ++) {
+		d.pad1[i] = i + 128;
+		d.pad2[i] = i;
+	}
 
-	nemu_assert(t.pad1[0] ==  128);
+	struct dummy t = fun(d);
 
+	for(i = 0; i < N; i ++) {
+		nemu_assert(t.pad1[i] == i + 128);
+		nemu_assert(t.pad2[i] == i);
+	}
+
+	nemu_assert(i == N);
 
 	HIT_GOOD_TRAP;
 
