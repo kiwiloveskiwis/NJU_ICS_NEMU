4a9a6007b6c94d4e307e837d6590fdc07c07f561
tracer-ics2015 <tracer@njuics.org>
2016-07-27 22:40:52 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  22:40:52 up 2 days, 22:05,  3 users,  load average: 0.00, 0.01, 0.05 79ae2dc225e4c0a42ba2bdeb44057e55d7e0c96

--- nemu/src/cpu/exec/arith/idiv-template.h
+++ nemu/src/cpu/exec/arith/idiv-template.h
@@ -3,15 +3,10 @@
 #define instr idiv
 
 static void do_execute() {
-	int64_t a;
+	int32_t a = reg_w(R_EAX);
 	int32_t b = (DATA_TYPE_S)op_src->val;
-#if DATA_BYTE == 1
-	a = (int16_t)reg_w(R_EAX);
-#else
-	a = ((int64_t)REG(R_EDX) << (DATA_BYTE * 8)) | (int64_t)REG(R_EAX);
-#endif
 	REG(R_EAX) = a / b;
-	REG(R_EDX) = a % b;
+	if(a % b !=0) REG(R_EDX) = a % b;
 
 	print_asm_template1();
 }
--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -65,7 +65,7 @@ make_group(group2_cl_v,
 /* 0xf6 */
 make_group(group3_b,
 	test_i2rm_b, inv, not_rm_b, neg_rm_b, 
-	mul_rm_b, imul_rm2a_b, inv, idiv_rm_v) // idiv_rm_b ?
+	mul_rm_b, imul_rm2a_b, inv, idiv_rm_v) // TODO: check idiv!!
 
 /* 0xf7 */
 make_group(group3_v,
