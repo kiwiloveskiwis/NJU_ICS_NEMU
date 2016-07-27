38a9ba7ed3bc1d1d41bf041294ea2ce83943985c
tracer-ics2015 <tracer@njuics.org>
2016-07-28 06:12:03 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  06:12:03 up 3 days,  5:37,  3 users,  load average: 0.00, 0.01, 0.05 a0fd79763c539c7d7fe59efe953c8ad54c3a387c

--- nemu/src/cpu/exec/control/ret-template.h
+++ nemu/src/cpu/exec/control/ret-template.h
@@ -5,7 +5,7 @@ static void do_execute(){
 	cpu.eip = swaddr_read(cpu.esp, 4) - 1 - DATA_BYTE;
 	cpu.esp += 4;
 	DATA_TYPE inc_adr = op_src->val ;
-	cpu.eip += inc_adr;
+	cpu.esp += inc_adr;
 	print_asm("ret 0x%x", inc_adr);
 }
 
