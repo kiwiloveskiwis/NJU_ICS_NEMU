b820a57030f7eaa3a9190076d8cf12ea5d92cad2
tracer-ics2015 <tracer@njuics.org>
2016-07-27 16:48:08 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  16:48:08 up 2 days, 16:13,  3 users,  load average: 0.00, 0.01, 0.05 c1bc679c2927b0299dc14f041c57f71888951918

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -4,7 +4,7 @@
 
 static void do_execute() {
 	DATA_TYPE result = op_dest->val - op_src->val;
-	cpu.CF = (((long long)op_dest->val - (long long)op_src->val ) >> 32) & 1;
+	cpu.CF = (((unsigned long long)op_dest->val - (unsigned long long)op_src->val ) >> 32) & 1;
 	cpu.OF = (MSB(op_dest->val) ^ MSB(op_src->val)) && (MSB(op_src->val) == MSB(result));
 	update_PZS(result)
 	print_asm_template2();
