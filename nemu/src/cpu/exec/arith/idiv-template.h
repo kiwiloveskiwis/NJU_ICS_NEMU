61e26a8bf9a2376fad06bf69bca11fcc711001fd
tracer-ics2015 <tracer@njuics.org>
2016-07-27 22:44:50 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  22:44:50 up 2 days, 22:09,  3 users,  load average: 0.00, 0.01, 0.05 937b05d9d11f123a89078579cceee16e21878c45

--- nemu/src/cpu/exec/arith/idiv-template.h
+++ nemu/src/cpu/exec/arith/idiv-template.h
@@ -3,8 +3,13 @@
 #define instr idiv
 
 static void do_execute() {
-	int32_t a = reg_w(R_EAX);
-	int32_t b = op_src->val;
+	int64_t a;
+	int32_t b = (DATA_TYPE_S)op_src->val;
+#if DATA_BYTE == 1
+	a = (int16_t)reg_w(R_EAX);
+#else
+	a = ((int64_t)REG(R_EDX) << (DATA_BYTE * 8)) | (int64_t)REG(R_EAX);
+#endif
 	REG(R_EAX) = a / b;
 	REG(R_EDX) = a % b;
 
--- nemu/src/cpu/exec/data-mov/cltd.h
+++ nemu/src/cpu/exec/data-mov/cltd.h
@@ -6,9 +6,7 @@ make_helper(cltd) {
 	if(op_src->size == 16) {
 		if(cpu.gpr[R_AX]._16 < 0) cpu.gpr[R_DX]._16 = 0xffff;
 		else cpu.gpr[R_DX]._16 = 0;
-	}else if(op_src->size == 32) {
-		cpu.edx = (cpu.eax < 0) ? 0xffffffff : 0;
-	}
+	}else cpu.edx = (cpu.eax < 0) ? 0xffffffff : 0;
 	print_asm("cltd");
 	return 1;
 
