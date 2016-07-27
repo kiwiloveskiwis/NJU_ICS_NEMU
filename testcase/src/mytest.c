dafa9812df9cbee435217d4b902b4b8c5604932d
tracer-ics2015 <tracer@njuics.org>
2016-07-28 01:21:48 +0800

    >  run 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  01:21:48 up 3 days, 46 min,  3 users,  load average: 0.00, 0.01, 0.05 51538cb0bfdfb2fe9ed01c1fb1cd976f2d78a89f

--- testcase/src/mytest.c
+++ testcase/src/mytest.c
@@ -4,9 +4,16 @@
 
 
 int main() {
-	nemu_assert(strcmp(strcat("a","b"), "ab") == 0);
+    char s1[] = "a";
+    char * s2 = "b";
 
-//passed
+    char str[20]; // = s1
+    char * test = strcpy(str, s1);
+    char * s= strcat(test, s2); //strcmp(s, "ab") == 0
+    
+    int c = strcmp(s, "ab");
+    nemu_assert(c == 0);	
+
 
 	HIT_GOOD_TRAP;
 	return 0;
