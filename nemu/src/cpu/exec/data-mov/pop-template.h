376ece3ab2e76a335d19f9a1533c8cda4520ba52
tracer-ics2015 <tracer@njuics.org>
2016-07-27 20:46:16 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  20:46:16 up 2 days, 20:11,  3 users,  load average: 0.00, 0.01, 0.05 7db305327c87427a1a5f85a7cdf64787559598f4

--- nemu/src/cpu/exec/data-mov/pop-template.h
+++ nemu/src/cpu/exec/data-mov/pop-template.h
@@ -4,7 +4,7 @@
 
 static void do_execute() {
 	DATA_TYPE top_of_stack = swaddr_read(cpu.esp, DATA_BYTE);
-	if(op_dest->type == OP_TYPE_REG) REG(op_dest->reg) = top_of_stack;
+	if(op_src->type == OP_TYPE_REG) REG(op_src->reg) = top_of_stack;
 	else OPERAND_W(op_dest, top_of_stack);
 	cpu.esp += DATA_BYTE;
 	print_asm_template1();
