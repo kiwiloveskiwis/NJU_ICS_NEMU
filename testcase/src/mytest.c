e622f59b9ee49d072e67c9dd969e0b3c99129a1c
tracer-ics2015 <tracer@njuics.org>
2016-07-27 22:02:01 +0800

    >  run 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  22:02:01 up 2 days, 21:27,  3 users,  load average: 0.00, 0.01, 0.05 f4dc034238b3a95e242a797e39793c23f57225f5

--- testcase/src/mytest.c
+++ testcase/src/mytest.c
@@ -1,24 +1,28 @@
 #include "trap.h"
 
 
-int cube(int n) {
-	return n * n * n;
-}
-
 int main() {
-	int n, n2, n1, n0;
-	for(n = 100; n < 0x100; n ++) {
-		n2 = n / 100;
-		n1 = (n / 10) % 10;
-		n0 = n % 10;
-		if(n == cube(n2) + cube(n1) + cube(n0)) {
-			nemu_assert(n == 153);
+	int n = 28, sum, i, k = 0;
+	for(n = 28; n < 29; n ++) {
+		sum = 0;
+		for(i = 1; i < n; i ++) {
+			if(n % i == 0) {
+				sum += i;
+			}
+		}
+
+		if(sum == n) {
+			nemu_assert(n == 28);
+			k ++;
 		}
 	}
 
+	nemu_assert(k == 1);
 
 	HIT_GOOD_TRAP;
 
 	return 0;
 }
+
+
 
