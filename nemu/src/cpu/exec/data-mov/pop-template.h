2e9859f41cd7df5d2595180ce0337ede3630f68b
tracer-ics2015 <tracer@njuics.org>
2016-07-27 20:48:11 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  20:48:11 up 2 days, 20:13,  3 users,  load average: 0.00, 0.01, 0.05 88f7f7c3df6ccb546ccf5ae0e952d4ac0b0c729

--- nemu/src/cpu/exec/data-mov/pop-template.h
+++ nemu/src/cpu/exec/data-mov/pop-template.h
@@ -4,7 +4,8 @@
 
 static void do_execute() {
 	DATA_TYPE top_of_stack = swaddr_read(cpu.esp, DATA_BYTE);
-	if(op_src->type == OP_TYPE_REG) REG(op_src->reg) = top_of_stack;
+	if(op_src->type == OP_TYPE_REG) REG(op_src->reg) = top_of_stack; 
+	// notice that the dest of pop_r_v is op_src
 	else OPERAND_W(op_dest, top_of_stack);
 	cpu.esp += DATA_BYTE;
 	print_asm_template1();
--- testcase/src/mytest.c
+++ testcase/src/mytest.c
@@ -11,7 +11,6 @@ int main() {
 		n2 = n / 100;
 		n1 = (n / 10) % 10;
 		n0 = n % 10;
-		nemu_assert(n==100);
 		if(n == cube(n2) + cube(n1) + cube(n0)) {
 			nemu_assert(n == 153);
 		}
