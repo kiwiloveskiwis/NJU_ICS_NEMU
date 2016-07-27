e7e54c6c34696fbd00ef9f7a1ca0529f0558b51b
tracer-ics2015 <tracer@njuics.org>
2016-07-28 06:32:43 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  06:32:43 up 3 days,  5:57,  3 users,  load average: 0.00, 0.01, 0.05 68633b1174c9f4d55ebec723ba3bacc2cbb928c3

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -5,18 +5,10 @@ make_helper(concat3(instr, _, SUFFIX)) {
 	int inc;
 	DATA_TYPE src;
 	inc = cpu.DF ? -DATA_BYTE : DATA_BYTE;
-	if(DATA_BYTE != 4) { // 8 or 16 // when data_byte = 8?
-		src = MEM_R(cpu.gpr[R_SI]._16);
-		MEM_W(cpu.gpr[R_DI]._16, src);
-		cpu.gpr[R_SI]._16 += inc;
-		cpu.gpr[R_DI]._16 += inc;
-	}
-	else if (DATA_BYTE == 4) {
 		src = MEM_R(cpu.gpr[R_ESI]._32);
 		MEM_W(cpu.gpr[R_DI]._32, src);
 		cpu.gpr[R_SI]._32 += inc;
 		cpu.gpr[R_DI]._32 += inc;
-	}
 	return 1;
 }
 #include "cpu/exec/template-end.h"
