4482d82da83b60536c3d2719219dfde9359ef3c5
tracer-ics2015 <tracer@njuics.org>
2016-07-28 08:35:50 +0800

    >  run 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  08:35:50 up 3 days,  8:00,  3 users,  load average: 0.00, 0.01, 0.05 9ffb884faf2c84cb17f28887d0e1f628875a4c89

--- testcase/src/mytest.c
+++ testcase/src/mytest.c
@@ -1,28 +1,24 @@
 #include "trap.h"
 
 long long mul(long long a,long long b) {
-		long long ans = a*b;
-			return ans;
+	long long ans = a*b;
+	return ans;
 }
 
 int a[] = { -1364082006, 1157693227, -2022600529, 301212728};
 long long ans[] = { 1860719719092984036LL, -1579188499418773362LL, 2758992986934981174LL,
-	 -410878862242972368LL, 1340253607841673529LL, -2341550933349917083LL,
-	  348711935091793256LL, 4090912899911079841LL, -609233022994333112LL, 90729107509201984LL};
+	-410878862242972368LL, 1340253607841673529LL, -2341550933349917083LL,
+	348711935091793256LL, 4090912899911079841LL, -609233022994333112LL, 90729107509201984LL};
 
-  /*#define NR_DATA (sizeof(test_data) / sizeof(test_data[0]))  
-   *   */ 
 
 int main() {
-		
 
-		nemu_assert(mul(a[0], a[0]) == ans[0]);
-							
-						/* nemu_assert(loop == NR_DATA * (NR_DATA + 1) / 2);
-						 * */
-							HIT_GOOD_TRAP;
+	nemu_assert(mul(a[0], a[0]) == 1860719719092984036LL);
+//	nemu_assert(mul(a[0], a[0]) == ans[0]);
+	
+	HIT_GOOD_TRAP;
 
-								return 0;
+	return 0;
 }
-			
+
 
