f10ec1e19786464746f49e53e33dcf95c5a15d5d
tracer-ics2015 <tracer@njuics.org>
2016-07-28 08:14:13 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  08:14:13 up 3 days,  7:39,  3 users,  load average: 0.00, 0.01, 0.05 fab050bcead2d47a9ee331d7282bd7e68d5939

--- nemu/src/cpu/exec/data-mov/movsx-template.h
+++ nemu/src/cpu/exec/data-mov/movsx-template.h
@@ -3,7 +3,7 @@
 #define instr movsw
 static void do_execute(){
 	int result = op_src->val & 0xffff;
-	result = ((result << 16) >> 16);
+	if(result >> 15) result |= 0xffff0000;
 	OPERAND_W(op_dest, result);
 	print_asm_template2();
 }
@@ -12,10 +12,8 @@ make_instr_helper(rm2r)
 
 #define instr movsb
 static void do_execute(){
-	int shift = 24;
 	DATA_TYPE result = op_src->val & 0xff;
-	result = ((result << shift) >> shift);
-	Log("%x" str(DATA_TYPE), result );
+	if(result >> 31) result |= 0xffffff00;
 	OPERAND_W(op_dest, result);
 	print_asm_template2();
 }
