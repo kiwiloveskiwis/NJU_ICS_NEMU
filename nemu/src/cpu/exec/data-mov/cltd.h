2b7418c146231a8a49e11491bbf723170a10ab6c
tracer-ics2015 <tracer@njuics.org>
2016-07-28 09:25:13 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  09:25:13 up 3 days,  8:50,  3 users,  load average: 0.00, 0.01, 0.05 2da4b5b1f7176fa419dd60f2c2ca3c5d31f7ed3a

--- nemu/src/cpu/exec/data-mov/cltd.h
+++ nemu/src/cpu/exec/data-mov/cltd.h
@@ -4,9 +4,9 @@
 
 make_helper(cltd) {
 	if(op_src->size == 16) {
-		if(cpu.gpr[R_AX]._16 < 0) cpu.gpr[R_DX]._16 = 0xffff;
+		if(cpu.gpr[R_AX]._16 >> 15) cpu.gpr[R_DX]._16 = 0xffff;
 		else cpu.gpr[R_DX]._16 = 0;
-	}else cpu.edx = (cpu.eax < 0) ? 0xffffffff : 0;
+	}else cpu.edx = (cpu.eax >> 31) ? 0xffffffff : 0;
 	print_asm("cltd");
 	return 1;
 
