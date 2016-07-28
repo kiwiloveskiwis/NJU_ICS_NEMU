640f239982fb50b0e83ccd4fd4bd75da83b5d63c
tracer-ics2015 <tracer@njuics.org>
2016-07-28 20:48:34 +0800

    >  run 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  20:48:34 up 3 days, 20:13,  3 users,  load average: 0.00, 0.01, 0.05 667004d32a0b2cd67e6fbaab9a185159137b502

--- testcase/src/quadratic-eq.c
+++ testcase/src/quadratic-eq.c
@@ -4,9 +4,13 @@
 /* solve 0.5x^2 + 0.5x - 0.5 = 0 */
 
 int main() {
-	FLOAT x = 0x14000;
-	FLOAT a = sqrt(x);
-	a++;
+	FLOAT a = f2F(0.5);
+	    FLOAT b = f2F(0.5);
+		    FLOAT c = f2F(-0.5);
+
+	FLOAT dt = F_mul_F(b, b) - F_mul_F(F_mul_int(a, 4), c);
+	FLOAT s = sqrt(f2F(dt));
+	s++;
 
 	HIT_GOOD_TRAP;
 	return 0;
