04680b6faf31ea7201f49f7a055eb645d0511720
tracer-ics2015 <tracer@njuics.org>
2016-07-28 22:06:39 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  22:06:40 up 3 days, 21:31,  3 users,  load average: 0.00, 0.01, 0.05 6e57e9c667d9d117aca1a8e496e68c347d20c839

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -1,13 +1,24 @@
 #include "cpu/exec/template-start.h"
 
 #define instr call
-
+// DATA_BYTE can only be 2 or 4
 static void do_execute(){
-	 DATA_TYPE inc_adr = op_src->val ;
-	 cpu.esp -= DATA_BYTE;
-	 swaddr_write(cpu.esp, DATA_BYTE, cpu.eip + DATA_BYTE + 1);
-	 cpu.eip += inc_adr;
-	 print_asm("call\t0x%x", cpu.eip + 1 + DATA_BYTE);
+	if(op_src->type == OP_TYPE_IMM) {	
+		DATA_TYPE inc_adr = op_src->val ;
+		cpu.esp -= DATA_BYTE;
+		swaddr_write(cpu.esp, DATA_BYTE, cpu.eip + DATA_BYTE + 1);
+		cpu.eip += inc_adr;
+		print_asm("call\t0x%x", cpu.eip + 1 + DATA_BYTE);
+	}
+	else {
+		int off;
+		if(op_src->type == OP_TYPE_REG) off = 2;
+		else off = 3; // Same as jmp
+		cpu.esp -= DATA_BYTE;
+		MEM_W(cpu.esp, cpu.eip + off);	
+		cpu.eip = op_src->val - off;
+
+	}
 }
 
 make_instr_helper(i)
