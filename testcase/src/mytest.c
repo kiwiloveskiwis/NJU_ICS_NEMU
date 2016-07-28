d44861aeb1ea555537db31ca6e590179fa95286e
tracer-ics2015 <tracer@njuics.org>
2016-07-28 08:53:09 +0800

    >  run 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  08:53:09 up 3 days,  8:18,  3 users,  load average: 0.00, 0.01, 0.05 76526b0adb77480d31a91bb837a1d9b841ee92f2

--- testcase/src/mytest.c
+++ testcase/src/mytest.c
@@ -5,9 +5,8 @@ long long mul(long long a,long long b) {
 	return ans;
 }
 
-int a[] = { 0x1000000};
-long long ans[] = { 0x1000000000000LL};
-
+int a[] = { 0x1000000, 0x2000000};
+long long ans[] = { 0x1000000000000LL, 0x4000000000000LL};
 /*#define NR_DATA (sizeof(test_data) / sizeof(test_data[0]))  
  *   */ 
 
