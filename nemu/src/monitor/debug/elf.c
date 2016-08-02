f3c84ea490ed08ea80dbede11714f6a8430a139e
tracer-ics2015 <tracer@njuics.org>
2016-08-02 20:07:53 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  20:07:53 up  4:50,  2 users,  load average: 0.00, 0.01, 0.05 e884cebbfc18388cc365c9f6154a71e5c7b2b705

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -30,6 +30,7 @@ void printsh() {
 }
 
 void load_elf_tables(int argc, char *argv[]) {
+	return;
 	int ret;
 	Assert(argc == 2, "run NEMU with format 'nemu [program]'");
 	exec_file = argv[1];
@@ -43,11 +44,11 @@ void load_elf_tables(int argc, char *argv[]) {
 
 	/* The first several bytes contain the ELF header. */
 	Elf32_Ehdr *elf = (void *)buf;
-	//char magic[] = {ELFMAG0, ELFMAG1, ELFMAG2, ELFMAG3};
+	char magic[] = {ELFMAG0, ELFMAG1, ELFMAG2, ELFMAG3};
 
 	/* Check ELF header */
-	//assert(memcmp(elf->e_ident, magic, 4) == 0);		// magic number
-	//assert(elf->e_ident[EI_CLASS] == ELFCLASS32);		// 32-bit architecture
+	assert(memcmp(elf->e_ident, magic, 4) == 0);		// magic number
+	assert(elf->e_ident[EI_CLASS] == ELFCLASS32);		// 32-bit architecture
 	assert(elf->e_ident[EI_DATA] == ELFDATA2LSB);		// littel-endianG1
 	assert(elf->e_ident[EI_VERSION] == EV_CURRENT);		// current version
 	assert(elf->e_ident[EI_OSABI] == ELFOSABI_SYSV || 	// UNIX System V ABI
