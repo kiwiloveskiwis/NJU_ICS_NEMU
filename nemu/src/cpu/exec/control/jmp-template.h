9e775b8ad05ecfaf97fca72bd65c3489614c71b8
tracer-ics2015 <tracer@njuics.org>
2016-08-02 21:47:02 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  21:47:02 up  6:29,  3 users,  load average: 0.08, 0.03, 0.05 2f2d0d52d096f7513f96296c64ac61de147261f6

--- nemu/src/cpu/exec/control/jmp-template.h
+++ nemu/src/cpu/exec/control/jmp-template.h
@@ -2,27 +2,23 @@
 
 #define instr jmp
 static void do_execute(){
-	if(op_src->type == OP_TYPE_IMM) {	
 		int inc_adr = op_src->val ;
 		cpu.eip += inc_adr;
 		if(DATA_BYTE == 2) cpu.eip = ((cpu.eip + DATA_BYTE + 1) & 0x0000ffff) - DATA_BYTE - 1;
 		// TODO : right way?
 		print_asm("jmp\t$0x%x", cpu.eip + DATA_BYTE + 1);
-	} else { //reg or mem
-		int off;
-		if(op_src->type == OP_TYPE_REG) off = 2;
-		else off = 3;
-		cpu.eip = op_src->val - off;
-		print_asm("jmp $0x%x", cpu.eip + off);
-	}
-	
 }
 
 make_instr_helper(i)
 #if DATA_BYTE == 2 || DATA_BYTE == 4
-make_instr_helper(rm)
+make_helper(concat5(instr,_,rm,_,SUFFIX))
+{
+	int len=concat4(decode_,rm,_,SUFFIX)(eip+1);
+	cpu.eip=op_src->val-len-1;
+	print_asm_template1();
+	return len+1;
+}
 #endif
-
 
 #include "cpu/exec/template-end.h"
 
