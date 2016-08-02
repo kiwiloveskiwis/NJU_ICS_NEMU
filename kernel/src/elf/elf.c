8a54b87170a34a375cb0373e1e25f6a6ae4eef73
tracer-ics2015 <tracer@njuics.org>
2016-08-02 16:32:07 +0800

    >  compile kernel 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  16:32:07 up  1:14,  3 users,  load average: 0.03, 0.02, 0.03 85fc72df61bf03d1ef978ac190199df532cb336d

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -43,14 +43,13 @@ uint32_t loader() {
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			 
+			set_bp();
 			ramdisk_read((uint8_t *)ph->p_vaddr, ELF_OFFSET_IN_DISK + ph->p_offset, ph->p_filesz);  
 			
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
 			memset((void *)(ph->p_vaddr + ph->p_filesz), 0, (ph->p_memsz - ph->p_filesz));
-
 
 			set_bp();
 
