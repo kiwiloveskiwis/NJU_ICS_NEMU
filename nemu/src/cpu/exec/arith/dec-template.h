c6377478fe86f72d524dbb98bc4e4babd1a07b9e
tracer-ics2015 <tracer@njuics.org>
2016-08-02 17:10:56 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  17:10:56 up  1:53,  2 users,  load average: 0.01, 0.04, 0.05 857235c844f0c2366940e0776a5275afc8b8314

--- nemu/src/cpu/exec/arith/dec-template.h
+++ nemu/src/cpu/exec/arith/dec-template.h
@@ -5,10 +5,9 @@
 static void do_execute () {
 	DATA_TYPE result = op_src->val - 1;
 	OPERAND_W(op_src, result);
-
-	/* TODO: Update EFLAGS. */
-	panic("please implement me");
-
+	cpu.CF = (op_src->val == 0);
+	cpu.OF = op_src->val == (1 << (DATA_BYTE * 8 - 1))? 1 : 0;
+	update_PZS(result)
 	print_asm_template1();
 }
 
