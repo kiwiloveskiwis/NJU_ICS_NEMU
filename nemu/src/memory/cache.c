035d722ae52562dd5b026eabc37b082e52626373
tracer-ics2015 <tracer@njuics.org>
2016-08-06 13:50:13 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  13:50:13 up  1:01,  3 users,  load average: 0.00, 0.01, 0.03 8f1152cc3c701dc89b9a321b7d2d4394d5e88b44

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -94,6 +94,7 @@ uint32_t cache_read(hwaddr_t addr, size_t len) { // len is handled in memory.c
 
 
 static void block_write(hwaddr_t addr, void *data, uint8_t *mask) {
+	Log("writing cache...");
 	Cache_Addr temp;
 	temp.value = addr & ~CACHE_MASK;
 	uint32_t tag = temp.tag;
