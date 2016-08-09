a9884842936fe2e20e91dbbd6ae4edc1a8e6bb38
tracer-ics2015 <tracer@njuics.org>
2016-08-09 20:44:31 +0800

    >  compile kernel 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  20:44:31 up 10:04,  3 users,  load average: 0.00, 0.01, 0.05 4f2e2f9a540114464109e3179d2d35eeb2ec52f9

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -44,9 +44,8 @@ uint32_t loader() {
 			uint32_t hwaddr = mm_malloc(ph->p_vaddr, ph->p_memsz);
 			ramdisk_read((uint8_t *)hwaddr, ph->p_offset, ph->p_filesz);
 			// memcpy((void *)hwaddr, (void *)ph->p_offset, ph->p_filesz);
-			// memset((void *)(hwaddr + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
+			memset((void *)(hwaddr + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
 			assert(0);
-
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
