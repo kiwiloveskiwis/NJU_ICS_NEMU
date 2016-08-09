0df18d4666f55dc0cec690f707df001b2bbfda36
tracer-ics2015 <tracer@njuics.org>
2016-08-09 14:12:01 +0800

    >  compile kernel 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  14:12:01 up  3:32,  3 users,  load average: 0.01, 0.02, 0.05 1f46466dbed37d2c2f9fa9324538e5b860d1b97d

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -19,13 +19,13 @@ uint32_t get_ucr3();
 uint32_t loader() {
 	Elf32_Ehdr *elf;
 	Elf32_Phdr *ph = NULL;
+	assert(0);
 
 	uint8_t buf[4096];
 
 #ifdef HAS_DEVICE
 	ide_read(buf, ELF_OFFSET_IN_DISK, 4096);
 #else
-	assert(0);
 	ramdisk_read(buf, ELF_OFFSET_IN_DISK, 4096);
 #endif
 
