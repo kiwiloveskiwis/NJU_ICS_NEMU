f9abcd918428589afdfe68e2b3a656b9a3c1e703
tracer-ics2015 <tracer@njuics.org>
2016-08-09 14:37:41 +0800

    >  compile kernel 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  14:37:41 up  3:57,  3 users,  load average: 0.00, 0.06, 0.12 45ac2d8a2ad99dd37ff6c9ccd92acc3b8cda3ad

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -80,8 +80,7 @@ void init_cond() {
 
 	/* Load the program. */
 
-	uint32_t eip = loader();
-	assert(0);
+	uint32_t eip = loader(); // ???
 	
 #if defined(IA32_PAGE) && defined(HAS_DEVICE)
 	/* Read data in the video memory to check whether 
