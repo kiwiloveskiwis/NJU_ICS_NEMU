80b99af725e879d0b9dff16f74dbf29580d1869c
tracer-ics2015 <tracer@njuics.org>
2016-08-09 21:19:14 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  21:19:14 up 10:39,  3 users,  load average: 0.00, 0.02, 0.05 f45f9b4a97035174ed6fcf5127cd894a1c0c06b2

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -15,11 +15,13 @@ void video_mapping_read_test();
 void video_mapping_clear();
 
 void init_cond();
+volatile x = 222;
 
 /* Initialization phase 1
  * The assembly code in start.S will finally jump here.
  */
 void init() {
+	x = x + 1;
 #ifdef IA32_PAGE
 	/* We must set up kernel virtual memory first because our kernel thinks it 
 	 * is located at 0xc0100000, which is set by the linking options in Makefile.
