6858b4535bcf0342a803e922822758cc9f458035
tracer-ics2015 <tracer@njuics.org>
2016-08-10 18:31:51 +0800

    >  compile kernel 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  18:31:51 up  1:36,  3 users,  load average: 0.02, 0.13, 0.12 23b2d730e11e1dec425503b6ce2da73cd088dc3b

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -39,10 +39,11 @@ uint32_t loader() {
 	ph = (Elf32_Phdr *) (void *)(buf + elf->e_phoff); 
 	for(; i < elf->e_phnum; i++) {
 		ph = (Elf32_Phdr *) (void *)(buf + elf->e_phoff + i * elf->e_phentsize); 
+		mm_malloc(ph->p_vaddr, (ph->p_memsz + 4096) & (~0xfff));
 		uint32_t hwaddr = mm_malloc(ph->p_vaddr, (ph->p_memsz + 4096) & (~0xfff));
-		/* Scan the program header table, load each segment into memory */
-			ramdisk_read((uint8_t *)hwaddr, ph->p_offset, ph->p_filesz);
-			memset((void *)(hwaddr + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
+		ramdisk_read((uint8_t *)hwaddr, ph->p_offset, ph->p_filesz);
+		memset((void *)(hwaddr + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
+
 		if(ph->p_type == PT_LOAD) {
 			
 
