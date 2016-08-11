66d49a8d350ebb4071e15eef04153b120a1e2da8
tracer-ics2015 <tracer@njuics.org>
2016-08-11 22:56:04 +0800

    >  compile kernel 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  22:56:04 up 15:46,  3 users,  load average: 0.00, 0.01, 0.05 433bad0af90f80e7a7b8c40b7234050f78ba2be

--- kernel/include/irq.h
+++ kernel/include/irq.h
@@ -11,6 +11,6 @@
 typedef struct TrapFrame {
 	uint32_t edi, esi, ebp, old_esp, ebx, edx, ecx, eax;
 	int32_t irq;
-	uint32_t error_code, eip, cs, eflags,
+	uint32_t error_code, eip, cs, eflags;
 } TrapFrame;
 #endif
