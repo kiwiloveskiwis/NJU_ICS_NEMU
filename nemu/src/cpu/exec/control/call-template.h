995ccdca24e4a23ed84e45e60179f08055a41978
tracer-ics2015 <tracer@njuics.org>
2016-08-12 20:32:04 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  20:32:04 up  4:05,  3 users,  load average: 0.01, 0.03, 0.05 9f4828f0ac03838ce1f604db286d4591fa27c1aa

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -1,14 +1,15 @@
 #include "cpu/exec/template-start.h"
 #define instr call
 
-static void do_execute(){
-		DATA_TYPE inc_adr = op_src->val ;
-		cpu.esp -= DATA_BYTE;
-		swaddr_write(cpu.esp, DATA_BYTE, cpu.eip + DATA_BYTE + 1, R_SS);
-		cpu.eip += inc_adr;
-		print_asm("call\t0x%x", cpu.eip + 1 + DATA_BYTE);
+make_helper(concat(call_i_,SUFFIX))
+{ 
+	int len = concat(decode_i_, SUFFIX)(eip+1);
+	cpu.esp -= 4;
+	MEM_W(cpu.esp, cpu.eip + len + 1, R_SS);
+	cpu.eip += op_src->val;
+	print_asm_template1();
+	return len + 1;
 }
-make_instr_helper(i)
 
 make_helper(concat(call_rm_,SUFFIX)) {
 	int len = concat(decode_rm_, SUFFIX)(eip + 1);
--- nemu/src/cpu/exec/system/out-template.h
+++ nemu/src/cpu/exec/system/out-template.h
@@ -3,14 +3,14 @@
 #define instr out
 make_helper(concat(out_a2i_, SUFFIX)) {
 	uint8_t port = instr_fetch(eip + 1, 1);
-	pio_write(port, DATA_BYTE, cpu.eax);
+	pio_write(port, DATA_BYTE, (DATA_TYPE)cpu.eax);
 	print_asm_template1();
 	return 2;
 }
 
 make_helper(concat(out_a2d_, SUFFIX)) {
 	uint16_t port = reg_w(R_DX);
-	pio_write(port, DATA_BYTE, cpu.eax);
+	pio_write(port, DATA_BYTE, (DATA_TYPE)cpu.eax);
 	print_asm_template1();
 	return 1;
 }
