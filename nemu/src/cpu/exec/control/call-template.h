5c35553e4d1e6a6e91f39bae7a36151ec400bd00
tracer-ics2015 <tracer@njuics.org>
2016-08-12 20:44:52 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  20:44:52 up  4:18,  3 users,  load average: 0.00, 0.01, 0.05 4952898557225eb663552d7074e82037b1b7a74

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -1,24 +1,24 @@
 #include "cpu/exec/template-start.h"
+
 #define instr call
-
-make_helper(concat(call_i_,SUFFIX))
-{ 
-	int len = concat(decode_i_, SUFFIX)(eip+1);
-	cpu.esp -= 4;
-	MEM_W(cpu.esp, cpu.eip + len + 1, R_SS);
-	cpu.eip += op_src->val;
-	print_asm_template1();
-	return len + 1;
+// DATA_BYTE can only be 2 or 4
+static void do_execute(){
+	if(op_src->type == OP_TYPE_IMM) {	
+		DATA_TYPE inc_adr = op_src->val ;
+		cpu.esp -= DATA_BYTE;
+		swaddr_write(cpu.esp, DATA_BYTE, cpu.eip + DATA_BYTE + 1, R_SS);
+		cpu.eip += inc_adr;
+		print_asm("call\t0x%x", cpu.eip + 1 + DATA_BYTE);
+	}
+	else {
+		int off = concat(decode_rm_, SUFFIX)(cpu.eip + 1); 
+		cpu.esp -= DATA_BYTE;
+		MEM_W(cpu.esp, cpu.eip + off, R_SS);
+		cpu.eip = op_src->val - off;
+	}
 }
 
-make_helper(concat(call_rm_,SUFFIX)) {
-	int len = concat(decode_rm_, SUFFIX)(eip + 1);
-	cpu.esp -= DATA_BYTE;
-	MEM_W(cpu.esp, cpu.eip + len + 1, R_SS);
+make_instr_helper(i)
+make_instr_helper(rm)
 
-	cpu.eip = op_src->val - len - 1;
-
-	print_asm_template1();
-	return len + 1;
-}
 #include "cpu/exec/template-end.h"
