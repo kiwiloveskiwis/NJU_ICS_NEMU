ba58f4d60c87922aab6f2b33051772b18afe288e
tracer-ics2015 <tracer@njuics.org>
2016-08-12 20:14:33 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  20:14:33 up  3:47,  3 users,  load average: 0.00, 0.01, 0.05 f803e1dea171a76dfbd9317e10ff9b9602b78

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -10,18 +10,16 @@ static void do_execute(){
 		cpu.eip += inc_adr;
 		print_asm("call\t0x%x", cpu.eip + 1 + DATA_BYTE);
 	}
-	else {
-		int off;
-		if(op_src->type == OP_TYPE_REG) off = 2;
-		else off = 3; // Same as jmp
-		cpu.esp -= DATA_BYTE;
-		MEM_W(cpu.esp, cpu.eip + off, R_SS);
-		cpu.eip = op_src->val - off;
-
-	}
 }
-
 make_instr_helper(i)
-make_instr_helper(rm)
 
+make_helper(concat(call_rm_,SUFFIX)) {
+	int len = concat(decode_rm_,SUFFIX)(eip+1);
+	cpu.esp -= DATA_BYTE;
+	MEM_W(cpu.esp, cpu.eip + len + 1, R_SS);
+	cpu.eip = op_src->val - len - 1;
+
+	print_asm_template1();
+	return len + 1;
+}
 #include "cpu/exec/template-end.h"
