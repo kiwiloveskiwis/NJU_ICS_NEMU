45dccdb3bb457acec30f7a10892e61557f8c26e4
tracer-ics2016 <tracer@njuics.org>
2016-11-26 19:42:12 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  19:42:12 up 1 day,  4:42,  3 users,  load average: 0.13, 0.05, 0.05 661eba5e41dc3e1a5c97505ea71ba6fb9eac096b

--- nemu/src/cpu/exec/arith/test-template.h
+++ nemu/src/cpu/exec/arith/test-template.h
@@ -1,21 +1,32 @@
 #include "cpu/exec/template-start.h"
 
-#define instr test
+#define instr test  
 
 static void do_execute() {
-	DATA_TYPE result = op_dest->val & op_src->val;
-//	OPERAND_W(op_dest, result); RESULT DISCARDED!!!!
-
-	cpu.CF = 0;
+	DATA_TYPE_S opl = op_dest->val;
+	DATA_TYPE_S opr = op_src->val;
+	DATA_TYPE_S result = opl&opr;
 	cpu.OF = 0;
-	update_PZS(result)
+	cpu.CF = 0;
+	cpu.SF = !!(result>>(DATA_BYTE*8-1));
+	cpu.ZF =!result;
+	int i, evenjudge;
+	for(i = 0; i < 7; i++) { 
+		result >>= 1;
+	}
+	evenjudge = result & 0x1;
+	cpu.PF = evenjudge;
 
-	print_asm_template2();
-}
 
+	print_asm_template2();	
+}	
+
+	make_instr_helper(i2r)
+	make_instr_helper(i2rm)
+	make_instr_helper(r2rm)
+	make_instr_helper(rm2r)
 make_instr_helper(i2a)
-make_instr_helper(i2rm)
-make_instr_helper(r2rm)
 
-#include "cpu/exec/template-end.h"
+
+#include "cpu/exec/template-end.h" 
 
