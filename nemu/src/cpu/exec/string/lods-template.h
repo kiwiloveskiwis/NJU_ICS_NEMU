6cea44388f110508038ef5aa82d1ab838c4c416f
tracer-ics2016 <tracer@njuics.org>
2016-11-26 23:33:35 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  23:33:35 up 1 day,  8:33,  4 users,  load average: 0.00, 0.01, 0.05 214dd8f845c941ab2c99822dd934c5eb8232715d

--- nemu/src/cpu/exec/string/lods-template.h
+++ nemu/src/cpu/exec/string/lods-template.h
@@ -5,7 +5,7 @@ make_helper(concat3(instr, _, SUFFIX)) {
 	concat(reg_, SUFFIX)(R_EAX) = MEM_R(cpu.esi);
 	int inc = cpu.DF ? -DATA_BYTE : DATA_BYTE;
 	cpu.esi += inc;
-	print_asm_template1();
+	print_asm("lods" str(SUFFIX));
 	return 1;
 }
 #include "cpu/exec/template-end.h"
