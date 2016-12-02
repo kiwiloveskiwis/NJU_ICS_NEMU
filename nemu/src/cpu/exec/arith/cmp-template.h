d24917c0901140833d4edee678a649ae91ae4628
tracer-ics2015 <tracer@njuics.org>
2016-12-02 15:19:23 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  15:19:23 up  2:12,  3 users,  load average: 0.52, 0.33, 0.19 ca88f54ebf21eb6dcc23d4b971a8b3257215a4d9

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -3,13 +3,21 @@
 #define instr cmp
 
 static void do_execute() {
+	uint32_t result;
+	update_sub(DATA_BYTE, op_dest->val, op_src->val, result);
+	result ++; // not used;
+	/*
 	DATA_TYPE result = op_dest->val - op_src->val;
 	cpu.CF = (((unsigned long long)op_dest->val - (unsigned long long)op_src->val ) >> 32) & 1;
 	cpu.OF = (MSB(op_dest->val) ^ MSB(op_src->val)) && (MSB(op_src->val) == MSB(result));
-	update_PZS(result)
+	update_PZS(result) */
 	print_asm_template2();
 }
-
+/*
+	uint32_t result;
+	update_sub(DATA_BYTE, op_dest->val, op_src->val, result);
+	result ++; // not used;
+*/
 	make_instr_helper(i2a)
 	make_instr_helper(i2rm)
 	make_instr_helper(r2rm)
