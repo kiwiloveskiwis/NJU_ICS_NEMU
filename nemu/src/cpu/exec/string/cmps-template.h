cef36617b875dc4a3743f6a219de378edd1edcd2
tracer-ics2016 <tracer@njuics.org>
2016-12-02 17:06:13 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  17:06:13 up  3:59,  5 users,  load average: 0.16, 0.06, 0.06 7b28ba449595dbf9da6207ad18ae855b6f807

--- nemu/src/cpu/exec/string/cmps-template.h
+++ nemu/src/cpu/exec/string/cmps-template.h
@@ -3,25 +3,11 @@
 
 make_helper(concat3(instr, _, SUFFIX)) {
 	int inc;
-	DATA_TYPE src, dest;
 	inc = cpu.DF ? -DATA_BYTE : DATA_BYTE;
-	if(DATA_BYTE != 4) { // 8 or 16
-		src = MEM_R(cpu.gpr[R_SI]._16);
-	   	dest = MEM_R(cpu.gpr[R_DI]._16);
-		cpu.gpr[R_SI]._16 += inc;
-		cpu.gpr[R_DI]._16 += inc;
-	}
-	else {
-		src = MEM_R(cpu.gpr[R_ESI]._32);
-	   	dest = MEM_R(cpu.gpr[R_EDI]._32);
-		cpu.gpr[R_SI]._32 += inc;
-		cpu.gpr[R_DI]._32 += inc;
-	}
+	cpu.esi += inc; 
+	cpu.edi += inc;
+	update_sub(MEM_R(cpu.esi), MEM_R(cpu.edi));
 	
-	DATA_TYPE result = src - dest;
-	cpu.CF = ((unsigned int)src < (unsigned int)dest ); 
-	cpu.OF = (MSB(dest) ^ MSB(src)) && (MSB(dest) == MSB(result));
-	update_PZS(DATA_BYTE, result);
 
 	// when data_byte = 8?
 	return 1;
