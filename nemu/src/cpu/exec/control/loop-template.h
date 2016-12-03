4a4f724f346c3baaaaeef82c7894ee1cb3fd76bf
tracer-ics2016 <tracer@njuics.org>
2016-12-04 01:49:34 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  01:49:34 up  6:27,  2 users,  load average: 0.00, 0.01, 0.05 4b1df16a5d406a517e190eea77bffefe4ca6bb0c

--- nemu/src/cpu/exec/control/loop-template.h
+++ nemu/src/cpu/exec/control/loop-template.h
@@ -2,11 +2,12 @@
 #include "cpu/exec/helper.h"
 
 #define loop_condition(name, cond) make_helper(concat(name, _b)) {  cpu.ecx--; \
-	int len = concat(decode_i_, SUFFIX)(eip + 1); \
 	int new_eip = cpu.eip + (DATA_TYPE_S)op_src->val; \
 	if(cond) cpu.edi = new_eip; \
 	print_asm("%s\t$0x%x",str(instr), new_eip + DATA_BYTE + 1);  \
-	return len + 1;} 
+	return 1;} 
+
+// int len = concat(decode_i_, SUFFIX)(eip + 1); 
 	 
 loop_condition(loop, 0);
 
