1c0b7aff86af987cc283f2f33bb92c35531d2113
tracer-ics2016 <tracer@njuics.org>
2016-12-06 01:03:22 +0800

    >  compile NEMU 151250104 kiwi Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt25-2 (2016-04-08) i686 GNU/Linux  01:03:22 up  4:27,  2 users,  load average: 0.02, 0.35, 0.47 bdbbbb617b448016c8f1c2a07de55628846b542

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -3,6 +3,9 @@
 #include "../../lib-common/x86-inc/mmu.h"
 #include "../include/device/mmio.h"
 
+extern uint32_t dram_read(hwaddr_t addr,size_t len);
+extern void dram_write(hwaddr_t addr,size_t len, uint32_t data);
+
 uint32_t cache_read(hwaddr_t, size_t);
 void cache_write(hwaddr_t, size_t, uint32_t);
 int32_t hwaddr_read(hwaddr_t addr, size_t len);
@@ -20,13 +23,15 @@ hwaddr_t page_translate(lnaddr_t addr) {
 int32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	int mm = is_mmio(addr);
 	if(mm != -1) return mmio_read(addr, len, mm);
-	else return cache_read(addr, len) & (~0u >> ((4 - len) << 3));
+	else return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
+	// else return cache_read(addr, len) & (~0u >> ((4 - len) << 3));
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
 	int mm = is_mmio(addr);
 	if(mm != -1) mmio_write(addr, len, data, mm);
-	else cache_write(addr, len, data);
+	else dram_write(addr, len, data);
+	// else cache_write(addr, len, data);
 }
 
 uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
@@ -45,8 +50,7 @@ void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data) {
 	hwaddr_write(hwaddr,len,data);
 }
 
-void load_seg_cache(uint8_t sreg)
-{
+void load_seg_cache(uint8_t sreg) {
 	lnaddr_t off_addr = (lnaddr_t)cpu.gdtr_base + 8 * cpu.sr[sreg].index; 
 	SegDesc d;
 	uint32_t *tmp = (uint32_t *)&d;
